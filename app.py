6372d681a5b3f40d93186e20a33d8cd56a49f011
Tyler Hunt <tyhunt82@users.noreply.github.com>
2024-03-15 20:34:37 +0000

    add tabs

--- app.py
+++ app.py
@@ -7,31 +7,13 @@ LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="Schematic App",
+        page_icon=":punch:",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    tb_setup, tb_schem, tb_inst = st.tabs(["Setup", "Schematic", "Instructions"])
 
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+   
 
 
 if __name__ == "__main__":

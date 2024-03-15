e18061fc7f4c1327af5d995f273e26956e5c6d01
Tyler Hunt <tyhunt82@users.noreply.github.com>
2024-03-15 20:40:13 +0000

    uop

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+from components import sb 
 
 LOGGER = get_logger(__name__)
+
 
 
 def run():
     st.set_page_config(
         page_title="Schematic App",
         page_icon=":punch:",
+        layout="wide",
+        initial_sidebar_state="expanded"
     )
+
+    sb.sidebar()
 
     tb_setup, tb_schem, tb_inst = st.tabs(["Setup", "Schematic", "Instructions"])
 
---
+++ components/sb.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def sidebar():
+    sb = st.sidebar()
+
+    with sb:
+        st.title('Schematic App')
+
+
+    return sb
